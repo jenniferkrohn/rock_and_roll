d15090b5af95c2cc7bb0402893fc73046d363590
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-02-12 11:38:31 -0500

    first commit

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+	belongs_to :artist
+	validates :title, presence: true
+	validates :artist_id, presence: true
+	validates :rating, presence: true
+	validates :title, uniqueness: true
+end
